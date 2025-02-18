dfb0c645a0acf296a00c82596b6a03e2aaf1ee1c
erickmjr <erickmellojr@gmail.com>
2025-02-18 20:41:21 -0300

    header inicial

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ function styles() {
     .pipe(gulp.dest('./dist/css'))
 }
 
-function images(){
+function images() {
     return gulp.src('./src/images/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./dist/images'));
@@ -25,6 +25,5 @@ function scripts() {
 exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-    gulp.watch('./src/images/**/*', gulp.parallel(images));
     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
