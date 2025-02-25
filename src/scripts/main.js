af67756b7fac89c5be631ebf0d665aafb3a04257
erickmjr <erickmellojr@gmail.com>
2025-02-24 22:47:44 -0300

    alteracao de nome da secao "catalog" para "highlight" + finalizacao da secao highlight

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
 
-    let carousel = $('.catalog__carousel')
-
+    let carousel = $('.highlight__carousel')
+    let buttonPrev = $('#buttonPrev');
+    let buttonNext = $('#buttonNext');
+    
+    
     carousel.slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: false,
         arrows: false,
         autoplay: false,
-        autoplaySpeed: 2000
+        autoplaySpeed: 2000,
+        infinite: false,
+        edgeFriction: 0,
+        draggable: false
     });
 
     $('#buttonPrev').click(function(){
@@ -18,4 +24,16 @@ $(document).ready(function(){
     $('#buttonNext').click(function(){
         carousel.slick('slickNext');
     });
-});
+
+    carousel.on('afterChange', function (event, slick, currentSlide) {
+        console.log(currentSlide)
+
+        if (currentSlide === 0) {
+            buttonPrev.parent().addClass('v--hidden');
+            buttonNext.parent().removeClass('v--hidden');
+        } else {
+            buttonNext.parent().addClass('v--hidden');
+            buttonPrev.parent().removeClass('v--hidden');
+        }
+    });
+}); 
