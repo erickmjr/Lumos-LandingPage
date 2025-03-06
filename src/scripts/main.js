7f8c48bdcf25debbf659e6aa3b1658a72f53af25
erickmjr <erickmellojr@gmail.com>
2025-03-05 22:36:28 -0300

    section mylist iniciada

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-    let carousel = $('.highlight__carousel');
-    let buttonPrev = $('#buttonPrev');
-    let buttonNext = $('#buttonNext');
+    let carouselHighlight = $('.highlight__carousel');
+    let buttonPrevHighlight = $('#buttonPrevHighlight');
+    let buttonNextHighlight = $('#buttonNextHighlight');
     
-    carousel.slick({
+    carouselHighlight.slick({
         slidesToShow: 4,
         slidesToScroll: 3,
         autoplay: false,
@@ -15,25 +15,58 @@ $(document).ready(function(){
         draggable: false
     });
 
-    buttonPrev.click(function(){
-        carousel.slick('slickPrev');
+    buttonPrevHighlight.click(function(){
+        carouselHighlight.slick('slickPrev');
     });
 
-    buttonNext.click(function(){
-        carousel.slick('slickNext');
+    buttonNextHighlight.click(function(){
+        carouselHighlight.slick('slickNext');
     });
 
-    carousel.on('afterChange', function (event, slick, currentSlide) {
+    carouselHighlight.on('afterChange', function (event, slick, currentSlide){
+        if (currentSlide === 0) {
+            buttonPrevHighlight.parent().addClass('v--hidden');
+            buttonNextHighlight.parent().removeClass('v--hidden');
+        } else if ((currentSlide >= 3) && (currentSlide < 6)) {
+            buttonPrevHighlight.parent().removeClass('v--hidden');
+            buttonNextHighlight.parent().removeClass('v--hidden');
+        } else { 
+            buttonNextHighlight.parent().addClass('v--hidden');
+            buttonPrevHighlight.parent().removeClass('v--hidden');
+        }
+    });
+
+    let carouselList = $('.mylist__carousel');
+    let buttonPrevList = $('#buttonPrevList');
+    let buttonNextList = $('#buttonNextList');
+    
+    carouselList.slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+        autoplay: false,
+        infinite: false,
+        edgeFriction: 0,
+        draggable: false
+    });
+
+    buttonPrevList.click(function(){
+        carouselList.slick('slickPrev');
+    });
+
+    buttonNextList.click(function(){
+        carouselList.slick('slickNext');
+    });
+
+    carouselList.on('afterChange', function (event, slick, currentSlide){
         console.log(currentSlide)
         if (currentSlide === 0) {
-            buttonPrev.parent().addClass('v--hidden');
-            buttonNext.parent().removeClass('v--hidden');
-        } else if ((currentSlide >= 3) && (currentSlide < 6)) {
-            buttonPrev.parent().removeClass('v--hidden');
-            buttonNext.parent().removeClass('v--hidden');
-        } else { 
-            buttonNext.parent().addClass('v--hidden');
-            buttonPrev.parent().removeClass('v--hidden');
+            buttonPrevList.parent().addClass('v--hidden');
+            buttonNextList.parent().removeClass('v--hidden');
+        } else if ((currentSlide > 0)) {
+            buttonPrevList.parent().removeClass('v--hidden');
+            buttonNextList.parent().addClass('v--hidden');
         }
     });
 
